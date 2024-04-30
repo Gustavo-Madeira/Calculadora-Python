d0cc03eb8254c455d6579cd13ec6bcac04a5a1c1
Gustavo Madeira <147175175+Gustavo-Madeira@users.noreply.github.com>
2024-04-30 10:03:10 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ class Calculadora:
         pass
     def soma(self, n1, n2):
         return n1 + n2
-    def subtrair(self, n1, n2):
+    def sub(self, n1, n2):
         return n1 - n2
-    def multi(self, n1, n2):
+    def mul(self, n1, n2):
         return n1 * n2
     def div(self, n1, n2):
         if n2 != 0:
             return n1 / n2
         else:
-            return 'ERRO: Não existe divisão por zero'
-
+            print('ERROR: Não existe divisão por 0(zero)')
 calcular = Calculadora()
-n1 = float(input('Digite o 1° número: '))
-n2 = float(input('Digete o 2° número: '))
 
-print(f'Soma = {calcular.soma(n1, n2)}\nSubtração = {calcular.subtrair(n1, n2)} \nMultiplicação = {calcular.multi(n1, n2)} \nDivisão = {calcular.div(n1, n2)}')
+print('O que você deseja calcular? 1- Soma / 2- Subtração / 3- Multiplicação / 4- Divisão')
+opcao = input('Escreva o número do que deseja calcular: ')
+
+if opcao == '1':
+   n1 = float(input('Digite o primeiro número: '))
+   n2 = float(input('Digite o segundo número: '))
+   print(f'A soma é {calcular.soma(n1, n2)}')
+elif opcao == '2':
+    n1 = float(input('Digite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+    print(f'A subtração é {calcular.sub(n1, n2)}')
+elif opcao == '3':
+    n1 = float(input('Digite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+    print(f'A multiplicação é {calcular.mul(n1, n2)}')
+elif opcao == '4':
+    n1 = float(input('Digite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+    print(f'A divisão é {calcular.div(n1, n2)}')
